973c19fdc2c4c92fad03b922cfa13c8cd755ca99
Marcelle Vargas <marcellevargas@Marcelle-Vargas-MacBook-Pro-464A.local>
2022-06-07 09:25:25 -0300

    Connecting with API

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.create({
+  baseURL: 'https://api.unsplash.com',  
+  headers: {
+    Authorization: "Client-ID S1tUvi3Buus3EFerw8TJ3PqbC0rgg6HZa-IdS6xgnKM",
+  },
+});
