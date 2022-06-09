1a1afe6db763d4341821acacafac9050fc13b2f3
Marcelle Vargas <marcellevargas@Marcelle-Vargas-MacBook-Pro-464A.local>
2022-06-09 16:40:07 -0300

    <fix> fix image card

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,18 +4,31 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
+    //Aparece como 0 por que o print é antes de carregar a imagem
+    // console.log(this.imageRef.current.clientHeight);
+
+    //Resolvendo o erro
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
 
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
   render() {
-    const { description, urls } = this.props.images;
+    const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
